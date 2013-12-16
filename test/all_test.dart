5ecba6f7a158cfcf82d32b8792290382e680db0f
Jamie C <jcouperwhite@gmail.com>
2013-12-16 04:33:37 +0000

    updated pubspec

---
+++ test/all_test.dart
@@ -0,0 +1,14 @@
+
+library all_tests;
+
+import 'range_test.dart' as range;
+import 'required_test.dart' as required;
+import 'validatable_test.dart' as validatable;
+
+void main() {
+
+  range.main();
+  required.main();
+  validatable.main();
+
+}
